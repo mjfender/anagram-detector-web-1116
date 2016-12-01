6f17435f89ec82fff43ebdc932db86de12c9ee2a
github username <github email address>
2016-12-01 08:18:56 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+attr_accessor :word
+attr_reader :match
+
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagrams)
+    @anagrams = anagrams
+    # get space separated string of anagrams and set to method variable
+
+    the_matches = @anagrams.collect do |anagram|
+    # iterate through array of anagram words
+    # collect the ones that match the converted/sorted word
+      if @word.split("").sort == anagram.split("").sort
+        anagram
+      end 
+    end
+    the_matches.compact
+  end
+
+end
